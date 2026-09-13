0c3034498edfc87cc929d654c2d109bc50bba44b
marziye moosavi <mousavi.m@sina.tums.ac.ir>
2026-09-13 08:24:16 +0000

    ffgf

--- tedg.py
+++ tedg.py
@@ -25,14 +25,18 @@ def add_note_1(action=None, success=None, container=None, results=None, handle=N
 
     id_value = container.get("id", None)
     format_1 = phantom.get_format_data(name="format_1")
+    format_1__as_list = phantom.get_format_data(name="format_1__as_list")
 
     parameters = []
 
-    parameters.append({
-        "content": format_1,
-        "title": "title1",
-        "container_id": id_value,
-    })
+    # build parameters list for 'add_note_1' call
+    for format_1__item in format_1__as_list:
+        if format_1__item is not None:
+            parameters.append({
+                "content": format_1,
+                "title": format_1__item,
+                "container_id": id_value,
+            })
 
     ################################################################################
     ## Custom Code Start
