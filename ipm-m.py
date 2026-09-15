1c38e56b15d73dcae6f70bf2c0846effe5271788
admin <mousavi.m@sina.tums.ac.ir>
2026-09-15 03:53:46 +0000

    wsa

--- ipm-m.py
+++ ipm-m.py
@@ -33,7 +33,33 @@ def ipm_1(action=None, success=None, container=None, results=None, handle=None, 
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="local/IPM", parameters=parameters, name="ipm_1")
+    phantom.custom_function(custom_function="local/IPM", parameters=parameters, name="ipm_1", callback=format_2)
+
+    return
+
+
+@phantom.playbook_block()
+def format_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_2() called")
+
+    template = """\"hi\"\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "ipm_1:custom_function_result.success"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_2")
 
     return
 
