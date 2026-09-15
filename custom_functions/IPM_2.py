482afbf7f4886aef0dfdbf564b4e6c2976a2c8ec
admin <mousavi.m@sina.tums.ac.ir>
2026-09-15 18:57:53 +0000

    qaz

--- custom_functions/IPM_2.py
+++ custom_functions/IPM_2.py
@@ -1,12 +1,12 @@
 def IPM_2(input_event=None, **kwargs):
     """
     Args:
-        input_event (CEF type: *)
+        input_event
     
     Returns a JSON-serializable object that implements the configured data paths:
         severity: List of supported event severity values
     """
     ############################ Custom Code Goes Below This Line #################################
     outputs = {}
-    outputs["severity"] = repr(kwargs)
+    outputs["severity"] = str(kwargs.get("input_event", ""))
     return outputs
