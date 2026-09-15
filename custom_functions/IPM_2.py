932a33d4eab0b6c90a7211912b71d0aa1a264262
admin <mousavi.m@sina.tums.ac.ir>
2026-09-15 07:37:01 +0000

    tgf

--- custom_functions/IPM_2.py
+++ custom_functions/IPM_2.py
@@ -1,38 +1,14 @@
-def IPM_2(event_message=None, **kwargs):
+def IPM_2(input_severity=None, **kwargs):
     """
     Args:
-        event_message (CEF type: *)
+        input_severity (CEF type: *)
     
     Returns a JSON-serializable object that implements the configured data paths:
-        severity_list: List of supported event severity values
+        seve: List of supported event severity values
     """
     ############################ Custom Code Goes Below This Line #################################
-    severity_map = {
-        "0": "Emergency",
-        "1": "Alert",
-        "2": "Critical",
-        "3": "Error",
-        "4": "Warning",
-        "5": "Notification",
-        "6": "Informational",
-        "7": "Debugging"
-    }
+    outputs = {}
 
-    severity = ""
-    severity_name = ""
-
-    if "-" in event_message:
-        parts = event_message.split("-")
-
-        if len(parts) >= 2:
-            severity = parts[1].split(":")[0]
-
-            if severity in severity_map:
-                severity_name = severity_map[severity]
-
-    outputs = {
-        "severity": severity,
-        "severity_name": severity_name
-    }
+    outputs["severity"] = "high"
 
     return outputs
