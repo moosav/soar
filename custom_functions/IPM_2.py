c60ab09681bd6f80aa8a9db6e66ba9941efd812f
admin <mousavi.m@sina.tums.ac.ir>
2026-09-15 07:18:44 +0000

    ws

--- custom_functions/IPM_2.py
+++ custom_functions/IPM_2.py
@@ -1,7 +1,7 @@
 def IPM_2(event_message=None, **kwargs):
     """
     Args:
-        event_message
+        event_message (CEF type: *)
     
     Returns a JSON-serializable object that implements the configured data paths:
         severity_list: List of supported event severity values
