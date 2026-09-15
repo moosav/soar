4cefdc89972f7d1f1c773311fb4cd67835eeaa07
admin <mousavi.m@sina.tums.ac.ir>
2026-09-15 19:05:45 +0000

    wsd

--- custom_functions/IPM_2.py
+++ custom_functions/IPM_2.py
@@ -8,5 +8,5 @@ def IPM_2(input_event=None, **kwargs):
     """
     ############################ Custom Code Goes Below This Line #################################
     outputs = {}
-    outputs["severity"] = str(kwargs.get("input_event", ""))
+    outputs["severity"] = str(kwargs)
     return outputs
