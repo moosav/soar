3b1e9bf4cfc4015708f326ca062293c98e030cd7
admin <mousavi.m@sina.tums.ac.ir>
2026-09-16 06:33:39 +0000

    azs

--- custom_functions/IPM_2.py
+++ custom_functions/IPM_2.py
@@ -1,16 +1,13 @@
 def IPM_2(**kwargs):
     """
     Returns a JSON-serializable object that implements the configured data paths:
-        json_output: List of supported event severity values
+        severity: List of supported event severity values
     """
     ############################ Custom Code Goes Below This Line #################################
-    import json
     outputs = {}
 
     severity = str(kwargs.get("input_event", ""))
 
-    outputs["json_output"] = json.dumps({
-    "severity": severity
-})
+    outputs["severity"] = severity
 
     return outputs
