1d0d6fead40bb4be0ff40ca1204846cf46a7d2b4
admin <mousavi.m@sina.tums.ac.ir>
2026-09-16 10:43:59 +0000

    edcs

--- ipm-3.py
+++ ipm-3.py
@@ -55,14 +55,12 @@ def add_note_1(action=None, success=None, container=None, results=None, handle=N
 def ipm_2_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("ipm_2_2() called")
 
-    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.id","artifact:*.id"], scope="new")
-
-    container_artifact_header_item_0 = [item[0] for item in container_artifact_data]
+    severity_value = container.get("severity", None)
 
     parameters = []
 
     parameters.append({
-        "input_event": container_artifact_header_item_0,
+        "input_event": severity_value,
     })
 
     ################################################################################
