068e79f1eb6003daf79bebadce07cfed542dd9bd
admin <mousavi.m@sina.tums.ac.ir>
2026-09-16 17:45:25 +0000

    wsa

--- custom_functions/IPM_2.py
+++ custom_functions/IPM_2.py
@@ -1,5 +1,8 @@
-def IPM_2(**kwargs):
+def IPM_2(artifact_input=None, **kwargs):
     """
+    Args:
+        artifact_input (CEF type: phantom artifact id)
+    
     Returns a JSON-serializable object that implements the configured data paths:
         severity: List of supported event severity values
     """
