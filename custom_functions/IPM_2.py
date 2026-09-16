73dccd5d8d25c6e15d42f4175ede3698cbc70855
admin <mousavi.m@sina.tums.ac.ir>
2026-09-16 03:35:28 +0000

    rfds

--- custom_functions/IPM_2.py
+++ custom_functions/IPM_2.py
@@ -4,8 +4,13 @@ def IPM_2(**kwargs):
         json_output: List of supported event severity values
     """
     ############################ Custom Code Goes Below This Line #################################
+    import json
     outputs = {}
+
     severity = str(kwargs.get("input_event", ""))
 
-    outputs["json_output"] = '{"severity":"' + severity + '"}'
+    outputs["json_output"] = json.dumps({
+    "severity": severity
+})
+
     return outputs
