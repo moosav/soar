86e13a9c38e652259672912e7070127b2cbf5188
admin <mousavi.m@sina.tums.ac.ir>
2026-09-16 05:55:12 +0000

    eds

--- ipm-3.py
+++ ipm-3.py
@@ -73,7 +73,82 @@ def ipm_2_1(action=None, success=None, container=None, results=None, handle=None
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="ipm/IPM_2", parameters=parameters, name="ipm_2_1", callback=add_note_1)
+    phantom.custom_function(custom_function="ipm/IPM_2", parameters=parameters, name="ipm_2_1", callback=decision_1)
+
+    return
+
+
+@phantom.playbook_block()
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("decision_1() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["ipm_2_1:custom_function_result.data.json_output", "==", "medium"]
+        ],
+        conditions_dps=[
+            ["ipm_2_1:custom_function_result.data.json_output", "==", "medium"]
+        ],
+        name="decision_1:condition_1",
+        delimiter=None)
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        add_note_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'elif' condition 2
+    found_match_2 = phantom.decision(
+        container=container,
+        conditions=[
+            ["ipm_2_1:custom_function_result.data.json_output", "==", "low"]
+        ],
+        conditions_dps=[
+            ["ipm_2_1:custom_function_result.data.json_output", "==", "low"]
+        ],
+        name="decision_1:condition_2",
+        delimiter=None)
+
+    # call connected blocks if condition 2 matched
+    if found_match_2:
+        add_note_2(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
+
+@phantom.playbook_block()
+def add_note_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("add_note_2() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    id_value = container.get("id", None)
+    ipm_2_1__result = phantom.collect2(container=container, datapath=["ipm_2_1:custom_function_result.data.json_output"])
+
+    parameters = []
+
+    # build parameters list for 'add_note_2' call
+    for ipm_2_1__result_item in ipm_2_1__result:
+        parameters.append({
+            "title": "title4",
+            "container_id": id_value,
+            "content": ipm_2_1__result_item[0],
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("add note", parameters=parameters, name="add_note_2", assets=["test-2"])
 
     return
 
