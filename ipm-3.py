e1c0bd01fadbc37ec9bf9edade2141fc2aef19f5
admin <mousavi.m@sina.tums.ac.ir>
2026-09-16 17:47:27 +0000

    wsa

--- ipm-3.py
+++ ipm-3.py
@@ -12,8 +12,8 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'ipm_2_2' block
-    ipm_2_2(container=container)
+    # call 'ipm_2_3' block
+    ipm_2_3(container=container)
 
     return
 
@@ -24,15 +24,15 @@ def add_note_1(action=None, success=None, container=None, results=None, handle=N
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
     id_value = container.get("id", None)
-    ipm_2_2__result = phantom.collect2(container=container, datapath=["ipm_2_2:custom_function_result.data.severity"])
+    ipm_2_3__result = phantom.collect2(container=container, datapath=["ipm_2_3:custom_function_result.data.severity"])
 
     parameters = []
 
     # build parameters list for 'add_note_1' call
-    for ipm_2_2__result_item in ipm_2_2__result:
+    for ipm_2_3__result_item in ipm_2_3__result:
         parameters.append({
             "title": "title3",
-            "content": ipm_2_2__result_item[0],
+            "content": ipm_2_3__result_item[0],
             "container_id": id_value,
         })
 
@@ -52,17 +52,17 @@ def add_note_1(action=None, success=None, container=None, results=None, handle=N
 
 
 @phantom.playbook_block()
-def ipm_2_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("ipm_2_2() called")
+def ipm_2_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("ipm_2_3() called")
 
-    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.id","artifact:*.id"], scope="new")
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.id","artifact:*.id"])
 
     container_artifact_header_item_0 = [item[0] for item in container_artifact_data]
 
     parameters = []
 
     parameters.append({
-        "input_event": container_artifact_header_item_0,
+        "artifact_input": container_artifact_header_item_0,
     })
 
     ################################################################################
@@ -75,7 +75,7 @@ def ipm_2_2(action=None, success=None, container=None, results=None, handle=None
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="ipm/IPM_2", parameters=parameters, name="ipm_2_2", callback=add_note_1)
+    phantom.custom_function(custom_function="ipm/IPM_2", parameters=parameters, name="ipm_2_3", callback=add_note_1)
 
     return
 
