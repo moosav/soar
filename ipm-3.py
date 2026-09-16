41e47b523faced4ed5ec6b1f1012c8eca6ca38c1
admin <mousavi.m@sina.tums.ac.ir>
2026-09-16 18:09:22 +0000

    wsa

--- ipm-3.py
+++ ipm-3.py
@@ -12,8 +12,8 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'ipm_2_5' block
-    ipm_2_5(container=container)
+    # call 'ipm_2_6' block
+    ipm_2_6(container=container)
 
     return
 
@@ -24,15 +24,15 @@ def add_note_1(action=None, success=None, container=None, results=None, handle=N
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
     id_value = container.get("id", None)
-    ipm_2_5__result = phantom.collect2(container=container, datapath=["ipm_2_5:custom_function_result.data.severity"])
+    ipm_2_6__result = phantom.collect2(container=container, datapath=["ipm_2_6:custom_function_result.data.severity"])
 
     parameters = []
 
     # build parameters list for 'add_note_1' call
-    for ipm_2_5__result_item in ipm_2_5__result:
+    for ipm_2_6__result_item in ipm_2_6__result:
         parameters.append({
             "title": "title3",
-            "content": ipm_2_5__result_item[0],
+            "content": ipm_2_6__result_item[0],
             "container_id": id_value,
         })
 
@@ -52,8 +52,8 @@ def add_note_1(action=None, success=None, container=None, results=None, handle=N
 
 
 @phantom.playbook_block()
-def ipm_2_5(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("ipm_2_5() called")
+def ipm_2_6(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("ipm_2_6() called")
 
     severity_value = container.get("severity", None)
 
@@ -73,7 +73,7 @@ def ipm_2_5(action=None, success=None, container=None, results=None, handle=None
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="ipm/IPM_2", parameters=parameters, name="ipm_2_5", callback=add_note_1)
+    phantom.custom_function(custom_function="ipm/IPM_2", parameters=parameters, name="ipm_2_6", callback=add_note_1)
 
     return
 
