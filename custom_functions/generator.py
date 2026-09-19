e574883d7aa0a46f66ab452515be8b52806fb42d
admin <mousavi.m@sina.tums.ac.ir>
2026-09-19 09:34:33 +0000

    wsax

--- custom_functions/generator.py
+++ custom_functions/generator.py
@@ -8,20 +8,10 @@ def generator(artifact_data=None, **kwargs):
     """
     ############################ Custom Code Goes Below This Line #################################
     
-    import re
-
     outputs = {}
 
     data = kwargs.get("artifact_data")
 
-    if data:
-        match = re.search(r'https?://[^\s"\']+', str(data))
-
-        if match:
-                outputs["url"] = match.group(0)
-        else:
-             outputs["url"] = ""
-    else:
-        outputs["url"] = ""
+    outputs["url"] = str(data)
 
     return outputs
