7473277cd10ae7ac9eeca0e9515b609c011a22ae
admin <mousavi.m@sina.tums.ac.ir>
2026-09-19 17:12:53 +0000

    qazwsx

--- custom_functions/generator.py
+++ custom_functions/generator.py
@@ -9,8 +9,6 @@ def generator(input_event=None, **kwargs):
     ############################ Custom Code Goes Below This Line #################################
     outputs = {}
 
-    raw = kwargs.get("input_event")
- 
-    outputs["url"] = "DEBUG_" + str(raw)
+    outputs["url"] = kwargs.get("input_event")
 
     return outputs
