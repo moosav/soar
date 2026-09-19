005ef796cd0e95c702cd4523a81ea8568f45470b
admin <mousavi.m@sina.tums.ac.ir>
2026-09-19 09:43:24 +0000

    wsa

--- custom_functions/generator.py
+++ custom_functions/generator.py
@@ -1,10 +1,10 @@
 def generator(artifact_data=None, **kwargs):
     """
     Args:
-        artifact_data
+        artifact_data (CEF type: *)
     
     Returns a JSON-serializable object that implements the configured data paths:
-        url
+        url (CEF type: *)
     """
     ############################ Custom Code Goes Below This Line #################################
     
