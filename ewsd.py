2de99b3e4f0c220228a7b36faa99463b79ec9aba
admin <mousavi.m@sina.tums.ac.ir>
2026-09-19 17:16:37 +0000

    wsxedc

---
+++ ewsd.py
@@ -0,0 +1,102 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+@phantom.playbook_block()
+def on_start(container):
+    phantom.debug('on_start() called')
+
+    # call 'generator_1' block
+    generator_1(container=container)
+
+    return
+
+@phantom.playbook_block()
+def generator_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("generator_1() called")
+
+    parameters = []
+
+    parameters.append({
+        "input_event": "TEST123",
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="ipm/generator", parameters=parameters, name="generator_1", callback=add_note_1)
+
+    return
+
+
+@phantom.playbook_block()
+def add_note_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("add_note_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    id_value = container.get("id", None)
+    generator_1__result = phantom.collect2(container=container, datapath=["generator_1:custom_function_result.data.url"])
+
+    parameters = []
+
+    # build parameters list for 'add_note_1' call
+    for generator_1__result_item in generator_1__result:
+        parameters.append({
+            "title": "title9",
+            "content": generator_1__result_item[0],
+            "container_id": id_value,
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("add note", parameters=parameters, name="add_note_1", assets=["test-2"])
+
+    return
+
+
+@phantom.playbook_block()
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # This function is called after all actions are completed.
+    # summary of all the action and/or all details of actions
+    # can be collected here.
+
+    # summary_json = phantom.get_summary()
+    # if 'result' in summary_json:
+        # for action_result in summary_json['result']:
+            # if 'action_run_id' in action_result:
+                # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
+                # phantom.debug(action_results)
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
