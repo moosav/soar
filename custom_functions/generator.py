2a5a441d6b4eae0a6cb71c9c469f4469e5472830
admin <mousavi.m@sina.tums.ac.ir>
2026-09-20 06:03:57 +0000

    rfvcd

--- custom_functions/generator.py
+++ custom_functions/generator.py
@@ -4,9 +4,9 @@ def generator(url_input=None, **kwargs):
         url_input
     
     Returns a JSON-serializable object that implements the configured data paths:
-        test_output (CEF type: *)
+        url (CEF type: *)
     """
     ############################ Custom Code Goes Below This Line #################################
     outputs = {}
-    outputs["test_output"] = str(url_input)
+    outputs["url"] = str(url_input)
     return outputs
