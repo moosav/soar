c721459b91e23c8c0f46e584f7e15e173b3db871
admin <mousavi.m@sina.tums.ac.ir>
2026-09-20 07:58:18 +0000

    rfdv

--- tgf.py
+++ tgf.py
@@ -18,6 +18,95 @@ def on_start(container):
     return
 
 @phantom.playbook_block()
+def geberator_2_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("geberator_2_1() called")
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.severity","artifact:*.id"], scope="new")
+
+    parameters = []
+
+    # build parameters list for 'geberator_2_1' call
+    for container_artifact_item in container_artifact_data:
+        parameters.append({
+            "severity_input": container_artifact_item[0],
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="ipm/Geberator_2", parameters=parameters, name="geberator_2_1", callback=geberator_2_1_callback)
+
+    return
+
+
+@phantom.playbook_block()
+def geberator_2_1_callback(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("geberator_2_1_callback() called")
+
+    
+    decision_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=filtered_artifacts, filtered_results=filtered_results)
+    decision_2(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=filtered_artifacts, filtered_results=filtered_results)
+
+
+    return
+
+
+@phantom.playbook_block()
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("decision_1() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["geberator_2_1:custom_function_result.data.severity", "==", "medium"]
+        ],
+        conditions_dps=[
+            ["geberator_2_1:custom_function_result.data.severity", "==", "medium"]
+        ],
+        name="decision_1:condition_1",
+        delimiter=None)
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        add_note_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
+
+@phantom.playbook_block()
+def decision_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("decision_2() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["geberator_2_1:custom_function_result.data.severity", "==", "high"]
+        ],
+        conditions_dps=[
+            ["geberator_2_1:custom_function_result.data.severity", "==", "high"]
+        ],
+        name="decision_2:condition_1",
+        delimiter=None)
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        add_note_2(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
+
+@phantom.playbook_block()
 def add_note_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("add_note_1() called")
 
@@ -52,17 +141,22 @@ def add_note_1(action=None, success=None, container=None, results=None, handle=N
 
 
 @phantom.playbook_block()
-def geberator_2_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("geberator_2_1() called")
+def add_note_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("add_note_2() called")
 
-    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.severity","artifact:*.id"], scope="new")
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    id_value = container.get("id", None)
+    geberator_2_1__result = phantom.collect2(container=container, datapath=["geberator_2_1:custom_function_result.data.severity"])
 
     parameters = []
 
-    # build parameters list for 'geberator_2_1' call
-    for container_artifact_item in container_artifact_data:
+    # build parameters list for 'add_note_2' call
+    for geberator_2_1__result_item in geberator_2_1__result:
         parameters.append({
-            "severity_input": container_artifact_item[0],
+            "title": "title_1",
+            "content": geberator_2_1__result_item[0],
+            "container_id": id_value,
         })
 
     ################################################################################
@@ -75,7 +169,7 @@ def geberator_2_1(action=None, success=None, container=None, results=None, handl
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="ipm/Geberator_2", parameters=parameters, name="geberator_2_1", callback=add_note_1)
+    phantom.act("add note", parameters=parameters, name="add_note_2", assets=["test-2"])
 
     return
 
