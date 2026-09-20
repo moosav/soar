c73bcfac5e9ff762d0a0cac9aca976a8a925dd2c
admin <mousavi.m@sina.tums.ac.ir>
2026-09-20 08:03:50 +0000

    qws

--- custom_functions/Geberator_2.py
+++ custom_functions/Geberator_2.py
@@ -1,10 +1,12 @@
-def Geberator_2(severity_input=None, **kwargs):
+def Geberator_2(severity_input=None, url_input=None, **kwargs):
     """
     Args:
         severity_input
+        url_input
     
     Returns a JSON-serializable object that implements the configured data paths:
         severity
+        url
     """
     ############################ Custom Code Goes Below This Line #################################
     import json
@@ -13,8 +15,9 @@ def Geberator_2(severity_input=None, **kwargs):
     outputs = {}
     
     
+    outputs["url"] = str(url_input)
     outputs["severity"] = str(severity_input)
-
+    
     # Return a JSON-serializable object
     assert json.dumps(outputs)  # Will raise an exception if the :outputs: object is not JSON-serializable
     return outputs
