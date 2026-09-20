ea7dd45cddf5687740405dcac4044cf4ccbf1eb4
admin <mousavi.m@sina.tums.ac.ir>
2026-09-20 04:53:06 +0000

    wsxax

--- tgf.py
+++ tgf.py
@@ -41,7 +41,41 @@ def generator_3(action=None, success=None, container=None, results=None, handle=
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="ipm/generator", parameters=parameters, name="generator_3")
+    phantom.custom_function(custom_function="ipm/generator", parameters=parameters, name="generator_3", callback=add_note_1)
+
+    return
+
+
+@phantom.playbook_block()
+def add_note_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("add_note_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    id_value = container.get("id", None)
+    generator_3__result = phantom.collect2(container=container, datapath=["generator_3:custom_function_result.data.url"])
+
+    parameters = []
+
+    # build parameters list for 'add_note_1' call
+    for generator_3__result_item in generator_3__result:
+        parameters.append({
+            "title": "title9",
+            "content": generator_3__result_item[0],
+            "container_id": id_value,
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("add note", parameters=parameters, name="add_note_1", assets=["test-2"])
 
     return
 
