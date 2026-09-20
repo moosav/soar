b74a1d88fbcdfc3e8a7edc0c9cde75dedca8591a
admin <mousavi.m@sina.tums.ac.ir>
2026-09-20 04:44:30 +0000

    wsxa

--- custom_functions/generator.py
+++ custom_functions/generator.py
@@ -1,11 +1,20 @@
-def generator(**kwargs):
+def generator(url_input=None, **kwargs):
     """
+    Args:
+        url_input
+    
     Returns a JSON-serializable object that implements the configured data paths:
         url (CEF type: *)
     """
     ############################ Custom Code Goes Below This Line #################################
     outputs = {}
 
-    outputs["url"] = kwargs.get("articat")
+    value = kwargs.get("url_input")
+
+    if value is not None:
+        outputs["url"] = str(value)
+    else:
+        outputs["url"] = ""
+
 
     return outputs
