3010662f3907e7388ddc752b64466b36027caeda
admin <mousavi.m@sina.tums.ac.ir>
2026-09-20 04:05:38 +0000

    wsas

--- tgf.py
+++ tgf.py
@@ -12,31 +12,10 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'generator_2' block
-    generator_2(container=container)
+    # call 'add_note_1' block
+    add_note_1(container=container)
 
     return
-
-@phantom.playbook_block()
-def generator_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("generator_2() called")
-
-    parameters = [{}]
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.custom_function(custom_function="ipm/generator", parameters=parameters, name="generator_2", callback=add_note_1)
-
-    return
-
 
 @phantom.playbook_block()
 def add_note_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
@@ -45,16 +24,17 @@ def add_note_1(action=None, success=None, container=None, results=None, handle=N
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
     id_value = container.get("id", None)
-    generator_2__result = phantom.collect2(container=container, datapath=["generator_2:custom_function_result.message"])
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.cef.destinationAddress","artifact:*.id"])
 
     parameters = []
 
     # build parameters list for 'add_note_1' call
-    for generator_2__result_item in generator_2__result:
+    for container_artifact_item in container_artifact_data:
         parameters.append({
             "title": "title5",
-            "content": generator_2__result_item[0],
+            "content": container_artifact_item[0],
             "container_id": id_value,
+            "context": {'artifact_id': container_artifact_item[1]},
         })
 
     ################################################################################
